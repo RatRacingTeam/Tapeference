b32b667b2b808f604586d0bc1b2a35c7a902375e
MurMeowHash <gavrishalex95@gmail.com>
2025-10-05 21:28:53 +0300

    :sparkles: implemented project bootstrap

---
+++ Assets/Features/BootstrapModule/Scripts/Installers/ProjectInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Features.CoroutineModule.Scripts.Installers;
+using UnityEngine;
+using Zenject;
+
+namespace Features.BootstrapModule.Scripts.Installers {
+    [CreateAssetMenu(fileName = nameof(ProjectInstaller), menuName = "Configurations/Bootstrap/Installers/" + nameof(ProjectInstaller))]
+    public class ProjectInstaller : ScriptableObjectInstaller {
+        public override void InstallBindings() {
+            CoroutineInstaller.Install(Container);
+        }
+    }
+}
---
+++ Assets/Features/BootstrapModule/Scripts/ProjectBootstrap.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Features.BootstrapModule.Scripts {
+    public class ProjectBootstrap : MonoBehaviour {
+        [SerializeField] private GameObject _projectUtilitiesObject;
+
+        private void Awake() =>
+            DontDestroyOnLoad(_projectUtilitiesObject);
+
+        private void Start() =>
+            Debug.LogError("Bootstrapping project");
+    }
+}
